0f5c4b24590dc66940b48bb3269549e3e0d1a49a
Yu Jaeseon <yujjjang3@gmail.com>
2020-05-11 23:30:55 +0900

    MySQL: minor issue: script

--- mysql/mysql-server-8.0/init_server.sh
+++ mysql/mysql-server-8.0/init_server.sh
@@ -3,7 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 cd $DIR
 
+DATADIR=$DIR/data/
 rm -rf data/
 
 # Initialize server
-./bin/mysqld --initialize-insecure
+./bin/mysqld --initialize-insecure --datadir=$DATADIR
--- mysql/mysql-server-8.0/run_server.sh
+++ mysql/mysql-server-8.0/run_server.sh
@@ -3,6 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 cd $DIR
 
+DATADIR=$DIR/data/
+
 rm -f logfile.err
 rm -f data/vseg.*
 rm -f data/undo_001
@@ -10,4 +12,4 @@ rm -f data/undo_002
 rm -f data/ib_logfile0
 rm -f data/ib_logfile1
 
-./run/bin/mysqld --log_error=$DIR/logfile.err --user=root
+./run/bin/mysqld --log_error=$DIR/logfile.err --datadir=$DATADIR --user=root
